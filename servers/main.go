86d962feb47b708bb55befa41cd6572e64e10312
Rashid Gizatullin <rashidg@halykbank.kz>
2024-11-15 12:59:03 +0500

    add tls

--- servers/main.go
+++ servers/main.go
@@ -11,5 +11,8 @@ func main() {
 		fmt.Fprintln(w, "Customer service")
 	})
 
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	// run the following command
+	// go run 'C:\Program Files\Go\src\crypto\tls\generate_cert.go' --host localhost
+	// to generate selfsigned cert.pem and key.pem for localhost
+	log.Fatal(http.ListenAndServeTLS(":3000", "./cert.pem", "./key.pem", nil))
 }
